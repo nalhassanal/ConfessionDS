7f33c671ff1df47e1018e2a1c766227ae802f9d1
Hassanal <u2102848@siswa.um.edu.my>
2022-05-26 01:41:44 +0800

    added the graph data structure

---
+++ AssignmentDS/src/main/java/dataStructure/WeightedGraph.java
@@ -0,0 +1,236 @@
+package dataStructure;
+import java.util.ArrayList;
+public class WeightedGraph <T extends Comparable<T>, N extends Comparable<N>>{
+    private Vertex<T, N> head;
+    private int size;
+
+    public WeightedGraph(){
+        head = null;
+        size = 0;
+    }
+
+    public void clear(){
+        head = null;
+    }
+
+    public int getSize(){
+        return this.size;
+    }
+
+    public int getIndeg(T vertexNode){
+        if (hasVertex(vertexNode)){
+            Vertex<T, N> curr = head;
+            while (curr != null){
+                if (curr.vertexObject.compareTo(vertexNode) == 0)
+                    return curr.indeg;
+                curr = curr.nextVertex;
+            }
+        }
+        return -1;
+    }
+
+    public int getOutdeg(T vertexNode){
+        if (hasVertex(vertexNode)){
+            Vertex<T, N> curr = head;
+            while (curr != null){
+                if (curr.vertexObject.compareTo(vertexNode) == 0)
+                    return curr.outdeg;
+                curr = curr.nextVertex;
+            }
+        }
+        return -1;
+    }
+
+    public boolean hasVertex(T vertexNode){
+        if (head == null)
+            return false;
+        Vertex<T, N> cur = head;
+        while (cur != null){
+            if (cur.vertexObject.compareTo(vertexNode) == 0)
+                return true;
+            cur = cur.nextVertex;
+        }
+        return false;
+    }
+
+    public boolean hasEdge(T src, T dst){
+        if (head == null)
+            return false;
+        if (!hasVertex(src) || !hasVertex(dst))
+            return false;
+        Vertex<T, N> cur = head;
+        while (cur != null){
+            if (cur.vertexObject.compareTo(src) == 0){
+                Edge<T, N> curEdge = cur.firstEdge;
+                while (curEdge != null){
+                    if (curEdge.dest.vertexObject.compareTo(dst) == 0)
+                        return true;
+                    curEdge = curEdge.nextEdge;
+                }
+            }
+            cur = cur.nextVertex;
+        }
+        return false;
+    }
+
+    public boolean addVertex(T vertexNode){
+        if (!hasVertex(vertexNode)){
+            Vertex <T, N> temp = head;
+            Vertex <T, N> newVertex = new Vertex<>(vertexNode, null);
+
+            if (head == null)
+                head = newVertex;
+            else {
+                Vertex<T, N> curr = head;
+                while (temp != null){
+                    curr = temp;
+                    temp = temp.nextVertex;
+                }
+                curr.nextVertex = newVertex;
+            }
+            size++;
+            return true;
+        }
+        return false;
+    }
+
+    public int getIndex(T vertexNode){
+        Vertex<T, N> curr = head;
+        int pos = 0;
+        while (curr != null){
+            if (curr.vertexObject.compareTo(vertexNode) == 0)
+                return pos;
+            curr = curr.nextVertex;
+            pos++;
+        }
+        return -1;
+    }
+
+    public T getVertex(int pos){
+        if (pos > size - 1 || pos < 0)
+            return null;
+        Vertex<T, N> curr = head;
+        for (int i = 0; i < pos; i++)
+            curr = curr.nextVertex;
+        return curr.vertexObject;
+    }
+
+    public ArrayList<T> getAllVertexObjects(){
+        ArrayList<T> ls = new ArrayList<>();
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            ls.add(curr.vertexObject);
+            curr = curr.nextVertex;
+        }
+        return ls;
+    }
+
+    public ArrayList<Vertex<T, N>> getAllVertices(){
+        ArrayList<Vertex<T, N>> ls = new ArrayList<>();
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            ls.add(curr);
+            curr = curr.nextVertex;
+        }
+        return ls;
+    }
+
+    public boolean addEdge(T src, T dst, N weight){
+        if (head == null)
+            return false;
+        if (!hasVertex(src) || !hasVertex(dst))
+            return false;
+
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            if (curr.vertexObject.compareTo(src) == 0){
+                Vertex<T, N> temp = head;
+                while (temp != null){
+                    if (temp.vertexObject.compareTo(dst) == 0){
+                        Edge<T, N> currEdge = curr.firstEdge;
+                        Edge<T, N> newEdge = new Edge<>(temp, weight, currEdge);
+                        curr.firstEdge = newEdge;
+                        curr.outdeg++;
+                        temp.indeg++;
+                        return true;
+                    }
+                    temp = temp.nextVertex;
+                }
+            }
+            curr = curr.nextVertex;
+        }
+        return false;
+    }
+
+    public boolean addUndirectedEdge(T src, T dst, N weight){
+        return addEdge(src, dst, weight) && addEdge(dst, src, weight);
+    }
+
+//    public Edge<T, N> removeEdge(T src, T dst){}
+
+//    public boolean removeEdge(T src, T dst){}
+
+    public N getEdgeWeight(T src, T dst){
+        if (head == null)
+            return null;
+        if (!hasVertex(src) || !hasVertex(dst))
+            return null;
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            if (curr.vertexObject.compareTo(src) == 0){
+                Edge<T, N> currEdge = curr.firstEdge;
+                while (currEdge != null){
+                    if (currEdge.dest.vertexObject.compareTo(dst) == 0)
+                        return currEdge.weight;
+                    currEdge = currEdge.nextEdge;
+                }
+            }
+            curr = curr.nextVertex;
+        }
+        return null;
+    }
+
+    public ArrayList<T> getNeighbours(T src){
+        if (!hasVertex(src))
+            return null;
+        ArrayList<T> ls = new ArrayList<>();
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            if (curr.vertexObject.compareTo(src) == 0){
+                Edge<T, N> currEdge = curr.firstEdge;
+                while (currEdge != null){
+                    ls.add(currEdge.dest.vertexObject);
+                    currEdge = currEdge.nextEdge;
+                }
+                break;
+            }
+            curr = curr.nextVertex;
+        }
+        return ls;
+    }
+
+    public void printEdges(){
+        Vertex<T, N> curr = head;
+        while (curr != null){
+            System.out.print("# " + curr.vertexObject + " : ");
+            Edge<T, N> currEdge = curr.firstEdge;
+            while (currEdge != null){
+                System.out.print("[" + curr.vertexObject + "," + currEdge.dest.vertexObject + "]");
+                currEdge = currEdge.nextEdge;
+            }
+            System.out.println();
+            curr = curr.nextVertex;
+        }
+        System.out.println();
+    }
+
+
+
+
+
+
+
+
+
+
+}
